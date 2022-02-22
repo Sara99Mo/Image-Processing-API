c3b83e6b544b67e3868c48601771bd332145426b
Sara Mohamed Al-Farghly <sma417650@gmail.com>
2022-02-22 09:40:01 +0200

    First Commit

---
+++ Image_Processing_API/src/routers/api/imageResize.ts
@@ -0,0 +1,78 @@
+import express, { Request, Response } from "express";
+import fs from "fs/promises";
+import path from "path";
+import imageHelper from "../../helper/imageHelper";
+import { Stats } from "fs";
+
+const imageResize = express.Router();
+
+imageResize.get('/' , async (req: Request , res: Response): Promise<void> => {
+    const filename = req.query['filename'];
+    const height = req.query['height'] ? parseInt(req.query['height'] as string, 10) : null;
+    const width = req.query['width'] ? parseInt(req.query['width'] as string, 10) : null;
+
+    // check if the query is correct
+    if (!filename || !height || !width) {
+        res.status(400).send('Please make sure url contains correct filename, height and width params');
+        return;
+    }
+
+    //get the full path from the filename
+    const pathFullImage = path.resolve(__dirname, `../../../assets/full/${filename}.jpg`);
+
+    //thumb path in the ${filename}-${width}x${height} format to save different size of images
+    const pathThumbImage = path.resolve(__dirname, `../../../assets/thumb/${filename}-${width}x${height}.jpg`);
+
+    //if the filename is exist
+    const fullImage: Stats | null = await fs.stat(pathFullImage).catch(() => {
+        res.status(404).send("Image doesn't exist");
+        return null;
+    });
+
+    if (!fullImage) {
+        return;
+    }
+
+     // Check if thumb was already created
+     const existThumb: Stats | null = await fs.stat(pathThumbImage).catch(() => {
+        return null;
+    });
+
+    if (existThumb) {
+        fs.readFile(pathThumbImage)
+            .then((thumbData: Buffer) => {
+                res.status(200).contentType('jpg').send(thumbData);
+            })
+            .catch(() => {
+                res.status(500).send('Error occured processing the image');
+            });
+    } else {
+        /*let htmlButton = `
+            <br><a href="/"><button 
+            style="
+                margin: 10px 2px 0;
+                color: #212;
+                background: #f56;
+                padding: 10px 20px; 
+                border-radius: 20%;
+                font-weight: 600;
+                font-size: larger;"
+            >Home</button></a>`;*/
+        // resize image
+        imageHelper
+            .resizeImage({
+                pathFullImage,
+                pathThumbImage,
+                height,
+                width,
+            })
+            .then((resizedImage: Buffer) => {
+                res.status(200).contentType('jpg').send(resizedImage);
+            })
+            .catch(() => {
+                res.status(500).send('Error occured processing the image');
+            });
+    }
+});
+
+export default imageResize;
---
+++ src/routers/api/listOfImages.ts
@@ -0,0 +1,64 @@
+import express, { Response, Request } from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+const listImagesRouter = express.Router();
+
+listImagesRouter.get('/', async (_req: Request, res: Response): Promise<void> => {
+    const folderPathFullImage = `${path.resolve(__dirname, '../../../assets/full')}`;
+
+    const files: string[] | null = await fs.readdir(folderPathFullImage).catch(() => {
+        res.status(500).send('Error occured reading the images');
+        return null;
+    });
+
+    if (!files) {
+        return;
+    }
+
+    let htmlResponse = `
+    <select name="filename" style="margin: 10px 2px; padding: 8px; width: 55%;">
+    <option>Select Image Name</option>
+    `;
+    
+
+
+    files.forEach((file: string): void => {
+        file = file.replace('.jpg' , '');
+        htmlResponse = htmlResponse + `<option>${file}</option>`;
+    });
+
+    let htmlForm = `
+        <h1 style="text-align:center; color:#258;">Image Processing API</h1>
+        <h3 style="text-align:center;">Please Enter filename, height and width and make sure url contains correct params</h3>
+        <form method="GET" action="api/images" 
+        style="    
+            border: 5px dotted #f56;
+            padding: 40px;
+            margin: 50px auto;
+            width: 500px;
+            text-align: center;
+            background: #1166118a;
+            border-radius: 20%;
+            box-shadow: 8px 7px 16px 3px;
+            font-size: x-large;">
+            Filename: <br>${htmlResponse}</select><br>
+            Width: <br><input type="number" name="width" style="margin: 10px 2px; padding: 8px; width: 55%;"><br>
+            Height: <br><input type="number" name="height" style="margin: 10px 2px; padding: 8px; width: 55%;"><br>
+            <button type="submit" 
+            style="
+                margin: 10px 2px 0;
+                color: #012;
+                background: #f56;
+                padding: 10px 20px;
+                border: none;
+                border-radius: 20%;
+                font-weight: 600;
+                font-size: smaller;"
+            >Submit</button>
+        </form>
+    `;
+    res.status(200).send(`${htmlForm}`);
+});
+
+export default listImagesRouter;
